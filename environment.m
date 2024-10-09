f0773a6522279616a247f44f6185a81cc6214cc4
matthewscm <claire.m.matthews@student.uts.edu.au>
2024-10-09 11:45:00 +1100

    Update environment.m

--- environment.m
+++ environment.m
@@ -12,7 +12,6 @@ classdef environment
             grid on;
 
             % Create and plot the first robot (Dobot)
-      
             robot1 = Dobot();  % Create an instance of the Dobot robot
           
             % Create and plot the second robot (6-DOF)
@@ -20,8 +19,9 @@ classdef environment
             robot2 = obj.createRobot(T_robot2);
             
             % Add the fence PLY model
-            obj.addFence(0.0008);  % Add fence at position (1, 1, 1)
-            obj.addFence2(0.0008);
+            obj.addFence(0.001);  % Add fence at position (1, 1, 1)
+            obj.addFence2(0.001);
+            obj.addTable(0.004);
 
             % Walls and floor
              % Add concrete floor
@@ -77,8 +77,8 @@ classdef environment
         
             % Desired final position of the fence
               desiredPositions = [
-                0, 1, 0.7
-                0, -1, 0.7  % Position for the second fence
+                0, 1.3, 1
+                0, -1.3, 1  % Position for the second fence
                 ];
               
             for i = 1:size(desiredPositions, 1)
@@ -122,8 +122,52 @@ classdef environment
         
             % Desired final position of the fence
               desiredPositions = [
-                1, 0, 0.7
-                -1, 0 , 0.7
+                1.3, 0, 1
+                -1.3, 0 , 1
+                ];
+              
+            for i = 1:size(desiredPositions, 1)
+                % Calculate the translation to move the fence to the desired position
+                translation = desiredPositions(i, :) - mean(v_rotated, 1);  % Translate to center around the desired position
+        
+                % Translate the vertices based on the calculated translation
+                v_translated = v_rotated + translation;  % Translate the vertices
+        
+                % Plot the fence using trisurf
+                trisurf(f, v_translated(:, 1), v_translated(:, 2), v_translated(:, 3), ...
+                        'FaceVertexCData', vertexColours, 'EdgeColor', 'interp', 'EdgeLighting', 'flat');
+            end
+        
+            % Set lighting to see colors of the bricks
+            lighting gouraud;
+          end
+
+          function addTable(obj, scale)
+            % Read the PLY file containing the brick's mesh
+            [f, v, data] = plyread('environment_files/Table.ply', 'tri');
+        
+            % Get the vertex colors if available
+            if isfield(data.vertex, 'red') && isfield(data.vertex, 'green') && isfield(data.vertex, 'blue')
+                vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+            else
+                % Default color if vertex colors are not available
+                vertexColours = repmat([0.8, 0.5, 0.5], size(v, 1), 1);  % Red color
+            end
+        
+            % Rotation matrix (90 degrees around z-axis)
+            R = [-1  0  0;
+                 0  0  1;
+                 0 1  0];
+
+            % Scale the vertices
+            v_scaled = v * scale;  % Scale the vertices by the given factor
+        
+            % Rotate the vertices
+            v_rotated = (R * v_scaled')';  % Rotate the vertices
+        
+            % Desired position of the table
+              desiredPositions = [
+                0, 0, 0.8
                 ];
               
             for i = 1:size(desiredPositions, 1)
