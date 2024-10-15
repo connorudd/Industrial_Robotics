e4bb59d16c9b39d7ba07efc04e3d4bd0d4960521
connorudd <160109432+connorudd@users.noreply.github.com>
2024-10-15 22:50:18 +0000

    Add files via upload

--- @SixDOFRobot/SixDOFRobot.m
+++ @SixDOFRobot/SixDOFRobot.m
@@ -1,50 +1,50 @@
-classdef SixDOFRobot
-    properties
-        % Joint parameters
-        jointAngles % Current joint angles (in radians)
-        linkLengths % Lengths of the robot links
-        robot % Robot model
+classdef SixDOFRobot < RobotBaseClass
+    %% UR3 Universal Robot 3kg payload robot model
+    %
+    % WARNING: This model has been created by UTS students in the subject
+    % 41013. No guarentee is made about the accuracy or correctness of the
+    % of the DH parameters of the accompanying ply files. Do not assume
+    % that this matches the real robot!
+
+    properties(Access = public)   
+        plyFileNameStem = 'SizDOFRobot';
     end
     
     methods
-        function obj = SixDOFRobot(linkLengths)
-            % Constructor to initialize link lengths and joint angles
-            obj.linkLengths = linkLengths;
-            obj.jointAngles = zeros(1, 6); % Initialize joint angles to zero
-            obj.robot = obj.createRobotModel();
-        end
-        
-        function robotModel = createRobotModel(obj)
-            % Define the links using DH parameters
-            L1 = Link('d',0.15185,'a',obj.linkLengths(1),'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
-            L2 = Link('d',0,'a',obj.linkLengths(2),'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
-            L3 = Link('d',0,'a',obj.linkLengths(3),'alpha',pi,'qlim', deg2rad([-360 360]), 'offset', 0);
-            L4 = Link('d',0.13105,'a',obj.linkLengths(4),'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
-            L5 = Link('d',0.08535,'a',obj.linkLengths(5),'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
-            L6 = Link('d',0.0921,'a',obj.linkLengths(6),'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
-            
-            % Create the serial link robot
-            robotModel = SerialLink([L1, L2, L3, L4, L5, L6], 'name', '6DOF Robot');
-            
+%% Constructor
+        function self = SizaDOFRobot(baseTr,useTool,toolFilename)
+            if nargin < 3
+                if nargin == 2
+                    error('If you set useTool you must pass in the toolFilename as well');
+                elseif nargin == 0 % Nothing passed
+                    baseTr = transl(0,0,0);  
+                end             
+            else % All passed in 
+                self.useTool = useTool;
+                toolTrData = load([toolFilename,'.mat']);
+                self.toolTr = toolTrData.tool;
+                self.toolFilename = [toolFilename,'.ply'];
+            end
+          
+            self.CreateModel();
+			self.model.base = self.model.base.T * baseTr;
+            self.model.tool = self.toolTr;
+            self.PlotAndColourRobot();
 
+            drawnow
         end
-        
-        function obj = updateJointAngles(obj, newAngles, baseTransform)
-            % Update the joint angles
-            obj.jointAngles = newAngles;
-        
-            % Set the robot's base transformation
-            obj.robot.base = baseTransform;
-            
-            % Plot the robot
-            obj.robot.plot(obj.jointAngles, 'noarrow');
-        
 
-        end
-        
-        function position = endEffectorPosition(obj)
-            % Calculate the end-effector position
-            position = obj.robot.fkine(obj.jointAngles);
-        end
+%% CreateModel
+        function CreateModel(self)
+            link(1) = Link('d',0.1,'a',0,'alpha',0,'qlim',deg2rad([-360 360]), 'offset',0);
+            link(2) = Link('d',0.1,'a',0,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
+            link(3) = Link('d',0.1,'a',0,'alpha',0,'qlim', deg2rad([-360 360]), 'offset', 0);
+            link(4) = Link('d',0.1,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
+            link(5) = Link('d',0.1,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
+            link(6) = Link('d',0.1,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
+            link(7) = Link('d',0.1,'a',0,'alpha',0,'qlim',deg2rad([-360 360]), 'offset',0);
+             
+            self.model = SerialLink(link,'name',self.name);
+        end      
     end
 end
