8fe2972961ce4589f20d7c6bdeb7e09c312ed03a
matthewscm <claire.m.matthews@student.uts.edu.au>
2024-10-16 11:57:52 +1100

    after class

--- @SixDOFRobot/SixDOFRobot.m
+++ @SixDOFRobot/SixDOFRobot.m
@@ -1,122 +1,156 @@
-% classdef SixDOFRobot < RobotBaseClass
-%     %% UR3 Universal Robot 3kg payload robot model
-%     properties(Access = public)   
-%         plyFileNameStem = 'SizDOFRobot';
+% classdef SixDOFRobot
+%     properties
+%         % Joint parameters
+%         jointAngles % Current joint angles (in radians)
+%         linkLengths % Lengths of the robot links
+%         robot % Robot model
+%         plyFileNameStem = 'SixDOFRobot';
 %     end
 % 
 %     methods
-% %% Constructor
-%         function self = SizaDOFRobot(baseTr,useTool,toolFilename)
-%             if nargin < 3
-%                 if nargin == 2
-%                     error('If you set useTool you must pass in the toolFilename as well');
-%                 elseif nargin == 0 % Nothing passed
-%                     baseTr = transl(0,0,0);  
-%                 end             
-%             else % All passed in 
-%                 self.useTool = useTool;
-%                 toolTrData = load([toolFilename,'.mat']);
-%                 self.toolTr = toolTrData.tool;
-%                 self.toolFilename = [toolFilename,'.ply'];
-%             end
-% 
-%             self.CreateModel();
-% 			self.model.base = self.model.base.T * baseTr;
-%             self.model.tool = self.toolTr;
-%             self.PlotAndColourRobot();
-% 
-%             drawnow
+%         function obj = SixDOFRobot(linkLengths)
+%             % Constructor to initialize link lengths and joint angles
+%             obj.linkLengths = linkLengths;
+%             obj.jointAngles = zeros(1, 6); % Initialize joint angles to zero
+%             obj.robot = obj.createRobotModel();
 %         end
 % 
-% %% CreateModel
-%         function CreateModel(self)
-%             link(1) = Link('d',0.1,'a',0,'alpha',0,'qlim',deg2rad([-360 360]), 'offset',0);
-%             link(2) = Link('d',0.1,'a',0,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
-%             link(3) = Link('d',0.1,'a',0,'alpha',0,'qlim', deg2rad([-360 360]), 'offset', 0);
-%             link(4) = Link('d',0.1,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
-%             link(5) = Link('d',0.1,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
-%             link(6) = Link('d',0.1,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
-%             link(7) = Link('d',0.1,'a',0,'alpha',0,'qlim',deg2rad([-360 360]), 'offset',0);
+%         function robotModel = createRobotModel(obj)
+%             % Define the links using DH parameters
+%             L1 = Link('d',0.15185,'a',obj.linkLengths(1),'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
+%             L2 = Link('d',0,'a',obj.linkLengths(2),'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
+%             L3 = Link('d',0,'a',obj.linkLengths(3),'alpha',pi,'qlim', deg2rad([-360 360]), 'offset', 0);
+%             L4 = Link('d',0.13105,'a',obj.linkLengths(4),'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
+%             L5 = Link('d',0.08535,'a',obj.linkLengths(5),'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
+%             L6 = Link('d',0.0921,'a',obj.linkLengths(6),'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
 % 
-%             self.model = SerialLink(link,'name',self.name);
-%         end      
+%             % Create the serial link robot
+%             robotModel = SerialLink([L1, L2, L3, L4, L5, L6], 'name', '6DOF Robot');
+% 
+%         end
+% 
+%         function obj = updateJointAngles(obj, newAngles, baseTransform)
+%             % Update the joint angles
+%             obj.jointAngles = newAngles;
+% 
+%             % Set the robot's base transformation
+%             obj.robot.base = baseTransform;
+% 
+%             % Plot the robot
+%             obj.robot.plot(obj.jointAngles, 'noarrow');
+% 
+% 
+%         end
+% 
+%         function position = endEffectorPosition(obj)
+%             % Calculate the end-effector position
+%             position = obj.robot.fkine(obj.jointAngles);
+%         end
 %     end
 % end
 
-classdef SixDOFRobot < RobotBaseClass
-    %% UR3 Universal Robot 3kg payload robot model
-    %
-    % WARNING: This model has been created by UTS students in the subject
-    % 41013. No guarantee is made about the accuracy or correctness of the
-    % DH parameters or the accompanying ply files. Do not assume
-    % that this matches the real robot!
-
-    properties(Access = public)   
-        plyFileNameStem = 'SizDOFRobotLink';  % Base name for ply files
+classdef SixDOFRobot
+    properties
+        % Joint parameters
+        jointAngles % Current joint angles (in radians)
+        linkLengths % Lengths of the robot links
+        robot       % Robot model
+        plyFileNameStem = 'SixDOFRobotLink'; % Base name for .ply files
     end
-    
+
     methods
-        %% Constructor
-        function self = SixDOFRobot(baseTr, useTool, toolFilename)
-            if nargin < 3
-                if nargin == 2
-                    error('If you set useTool you must pass in the toolFilename as well');
-                elseif nargin == 0 % Nothing passed
-                    baseTr = transl(0,0,0);  
-                end             
-            else % All passed in 
-                self.useTool = useTool;
-                toolTrData = load([toolFilename,'.mat']);
-                self.toolTr = toolTrData.tool;
-                self.toolFilename = [toolFilename,'.ply'];
-            end
-          
-            self.CreateModel();
-            self.model.base = self.model.base.T * baseTr;
-            self.model.tool = self.toolTr;
-            self.PlotAndColourRobot();
-
-            drawnow
+        function obj = SixDOFRobot(linkLengths)
+            % Constructor to initialize link lengths and joint angles
+            obj.linkLengths = linkLengths;
+            obj.jointAngles = zeros(1, 6); % Initialize joint angles to zero
+            obj.robot = obj.createRobotModel();
         end
 
-        %% CreateModel
-        function CreateModel(self)
-            link(1) = Link('d',0.1,'a',0,'alpha',0,'qlim',deg2rad([-360 360]), 'offset',0);
-            link(2) = Link('d',0.1,'a',0,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
-            link(3) = Link('d',0.1,'a',0,'alpha',0,'qlim', deg2rad([-360 360]), 'offset', 0);
-            link(4) = Link('d',0.1,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
-            link(5) = Link('d',0.1,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
-            link(6) = Link('d',0.1,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
-            link(7) = Link('d',0.1,'a',0,'alpha',0,'qlim',deg2rad([-360 360]), 'offset',0);
-             
-            self.model = SerialLink(link,'name','SixDOFRobot');
+        function robotModel = createRobotModel(obj)
+            % Define the links using DH parameters
+            L1 = Link('d', 0.15185, 'a', obj.linkLengths(1), 'alpha', pi/2, 'qlim', deg2rad([-360 360]), 'offset', 0);
+            L2 = Link('d', 0, 'a', obj.linkLengths(2), 'alpha', 0, 'qlim', deg2rad([-360 360]), 'offset', 0);
+            L3 = Link('d', 0, 'a', obj.linkLengths(3), 'alpha', pi, 'qlim', deg2rad([-360 360]), 'offset', 0);
+            L4 = Link('d', 0.13105, 'a', obj.linkLengths(4), 'alpha', pi/2, 'qlim', deg2rad([-360 360]), 'offset', 0);
+            L5 = Link('d', 0.08535, 'a', obj.linkLengths(5), 'alpha', -pi/2, 'qlim', deg2rad([-360 360]), 'offset', 0);
+            L6 = Link('d', 0.0921, 'a', obj.linkLengths(6), 'alpha', 0, 'qlim', deg2rad([-360 360]), 'offset', 0);
+
+            % Create the serial link robot
+            robotModel = SerialLink([L1, L2, L3, L4, L5, L6], 'name', '6DOF Robot');
         end
-        
-        %% PlotAndColourRobot
-        function PlotAndColourRobot(self)
-            for linkIndex = 0:self.model.n
-                % Get the ply filename for each link
-                plyFilePath = fullfile('path_to_ply_files', [self.plyFileNameStem, num2str(linkIndex), '.ply']);
-                if exist(plyFilePath, 'file') == 2  % Check if the file exists
-                    [faceData, vertexData, plyData] = plyread(plyFilePath, 'tri');
-                    
-                    % Scale the colours to be 0-to-1 (if ply files contain RGB info)
-                    vertexColours = [plyData.vertex.red, plyData.vertex.green, plyData.vertex.blue] / 255;
-                    
-                    % Plot the link
-                    self.model.faces{linkIndex+1} = faceData;
-                    self.model.points{linkIndex+1} = vertexData;
-                    
-                    % Attach patch objects to the robot
-                    patch('Faces', faceData, 'Vertices', vertexData, ...
-                          'FaceVertexCData', vertexColours, ...
-                          'EdgeColor', 'interp', 'FaceColor', 'interp');
-                else
-                    error(['PLY file not found: ', plyFilePath]);
-                end
-            end
+
+        function obj = updateJointAngles(obj, newAngles, baseTransform)
+            % Update the joint angles
+            obj.jointAngles = newAngles;
+
+            % Set the robot's base transformation
+            obj.robot.base = baseTransform;
+
+            % Plot the robot
+            obj.robot.plot(obj.jointAngles, 'noarrow');
+
+            % Attach .ply models to the links
+            obj.attachPlyModels();
         end
+
+function attachPlyModels(obj)
+    % Attach the .ply models to each link of the robot
+    for i = 1:length(obj.robot.links)
+        % Generate the filename for the corresponding .ply file
+        plyFile = sprintf('%s%d.ply', obj.plyFileNameStem, i);
+
+        % Read the .ply file
+        [f, v, data] = plyread(plyFile, 'tri');
+
+        % Compute the transformation matrix for the current link using fkine
+        tr = obj.robot.fkine(obj.jointAngles); % Get the transformation matrix for the end effector
+        % Print out the transformation matrix
+        disp(['Transformation matrix for link ', num2str(i), ':']);
+        disp(tr);
+        disp(size(tr));
+
+        % Assuming the transformation for each link can be computed as:
+        % Get the transformation matrix for link i
+        tr_link = obj.robot.A(i, obj.jointAngles); % Use fkine instead of A if necessary
+
+        % Check if the transformation is in 4x4 homogeneous form
+        if size(tr_link, 1) == 4
+            % Apply the transformation directly to vertices in homogeneous coordinates
+            v_hom = [v, ones(size(v, 1), 1)]';  % Convert to homogeneous coordinates
+            transformedVertices_hom = tr_link * v_hom; % Apply transformation
+            transformedVertices = transformedVertices_hom(1:3, :)';  % Convert back to Cartesian
+        else
+            % Assuming the transformation is in the form of [R; t]
+            R = tr_link(1:3, 1:3);  % Rotation matrix
+            t = tr_link(1:3, 4);    % Translation vector
+            transformedVertices = (R * v')' + t';  % Apply rotation and translation
+        end
+
+        % Extract color data (if available) and normalize the values
+        if isfield(data, 'vertex') && isfield(data.vertex, 'red')
+            vertexColors = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+        else
+            vertexColors = repmat([0.8, 0.8, 0.8], size(v, 1), 1); % Default to gray if no color info
+        end
+
+        % Plot the .ply model using patch
+        patch('Vertices', transformedVertices, 'Faces', f, ...
+              'FaceVertexCData', vertexColors, 'FaceColor', 'interp', 'EdgeColor', 'none');
+
+        hold on;
     end
+
+    axis equal;
+    hold off;
 end
 
 
+
+
+
+        function position = endEffectorPosition(obj)
+            % Calculate the end-effector position
+            position = obj.robot.fkine(obj.jointAngles);
+        end
+    end
+end
