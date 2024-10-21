ea37b0217dff179e31ceb59cfe3706527c715b7e
matthewscm <claire.m.matthews@student.uts.edu.au>
2024-10-21 14:11:29 +1100

    finishing environment

--- environment_files/test_ply.m
+++ environment_files/test_ply.m
@@ -1,5 +1,5 @@
 % Read the PLY file using a built-in function
-[f, v, data] = plyread('SixDOFRobotLink3.ply', 'tri');
+[f, v, data] = plyread('estop.ply', 'tri');
 vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
 
 % Display the mesh
