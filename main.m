fda0354b5365b272583ef9fa05b670e9576dc829
matthewscm <claire.m.matthews@student.uts.edu.au>
2024-10-25 11:03:41 +1100

    update

--- main.m
+++ main.m
@@ -15,25 +15,25 @@ classdef main
             robot2 = SixDOFRobot();
 
             %% robot view 
-            axis([-2 2 -2 2 0 3]); 
-            % axis equal;
+            % axis([-2 2 -2 2 0 3]); 
+             axis equal;
             view(3);
             %% moving 6DOF
             % robot2.model.teach(); 
-            % plate #1 location 
-            robot2.moveToTarget([0.3, -0.5, 1.0]);
-            % bowl
-            robot2.moveToTarget([0, 0, 1.05]);
-            % plate 2
-            robot2.moveToTarget([0.55, -0.5, 1.0]);
-            % bowl
-            robot2.moveToTarget([0, 0, 1.05]);
-            % plate 3
-            robot2.moveToTarget([0.7, -0.5, 1.0]);
-            % bowl
-            robot2.moveToTarget([0, 0, 1.05]);
-            % oven location 
-            robot2.moveToTarget([-0.13, 0.45, 1.3]);
+            % % plate #1 location 
+            % robot2.moveToTarget([0.3, -0.5, 1.0]);
+            % % bowl
+            % robot2.moveToTarget([0, 0, 1.05]);
+            % % plate 2
+            % robot2.moveToTarget([0.55, -0.5, 1.0]);
+            % % bowl
+            % robot2.moveToTarget([0, 0, 1.05]);
+            % % plate 3
+            % robot2.moveToTarget([0.7, -0.5, 1.0]);
+            % % bowl
+            % robot2.moveToTarget([0, 0, 1.05]);
+            % % oven location 
+            % robot2.moveToTarget([-0.13, 0.45, 1.3]);
 
             
             pause;
