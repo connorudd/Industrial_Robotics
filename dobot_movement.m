03a7d187aa8fe677d9b2729ab6b0552792bf7547
connorudd <160109432+connorudd@users.noreply.github.com>
2024-10-29 20:26:50 +1100

    Update dobot_movement.m

--- dobot_movement.m
+++ dobot_movement.m
@@ -1,85 +1,307 @@
-classdef dobot_movement
-    methods(Static)
+%% Workspace Setup Function
+clf;
+clc;
+% Create and plot robot
+robot = DobotMagician();
+q0 = [0, pi/6, pi/4, pi/2, 0];
+workspace = [-0.4, 0.4, -0.4, 0.4, 0, 0.4];
+scale = 0.5;
+robot.model.plot(q0,'workspace',workspace,'scale',scale);
+axis(workspace);
+hold on;
 
-        % A method to pick up 3 objects and place them in a line.
-        function applyToppings(robot1, pickUpPositions, placePositions, obj)
+%% Plot and detect color for green square
+[f, v, data] = plyread('green_square.ply', 'tri');
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+square_start_position = [0.25, 0, 0];
+square_scale_factor = 0.002;
+square_v_scaled = v * square_scale_factor;
+square_v_transformation = square_v_scaled + square_start_position;
+square1 = trisurf(f, square_v_transformation(:, 1), square_v_transformation(:, 2), square_v_transformation(:, 3), ...
+    'FaceVertexCData', vertexColours, ...
+    'FaceColor', 'interp', ...
+    'EdgeColor', 'none');
 
-            % Horizontal offset for each placed object along the X-axis
-            offset = 0.1;
+[f, v, data] = plyread('green_square.ply', 'tri');
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+square_start_position2 = [0.25, 0, 0];
+square_scale_factor2 = 0.002;
+square_v_scaled2 = v * square_scale_factor2;
+square_v_transformation2 = square_v_scaled2 + square_start_position;
+square2 = trisurf(f, square_v_transformation2(:, 1), square_v_transformation2(:, 2), square_v_transformation2(:, 3), ...
+    'FaceVertexCData', vertexColours, ...
+    'FaceColor', 'interp', ...
+    'EdgeColor', 'none');
+hideObj(square2);
 
-            % Loop to pick and place 3 objects
-            for i = 1:3
-                % Get the current object's pick-up position
-                targetPickUpPosition = pickUpPositions(i, :);
+[f, v, data] = plyread('green_square.ply', 'tri');
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+square_start_position3 = [0, 0.25, 0];
+square_scale_factor3 = 0.002;
+square_v_scaled3 = v * square_scale_factor3;
+square_v_transformation3 = square_v_scaled3 + square_start_position3;
+square3 = trisurf(f, square_v_transformation3(:, 1), square_v_transformation3(:, 2), square_v_transformation3(:, 3), ...
+    'FaceVertexCData', vertexColours, ...
+    'FaceColor', 'interp', ...
+    'EdgeColor', 'none');
+hideObj(square3);
 
-                 % Move above the pick-up position to approach safely
-                dobot_movement.moveToPosition(robot1, targetPickUpPosition + [0, 0, 0.1]);
-                pause(0.5);
-                
-                % Move to the exact pick-up position to grab the object
-                dobot_movement.moveToPosition(robot1, targetPickUpPosition);
-                pause(0.5);
-                
-                % Hides first object and reveals object at end effector
-                dobot_movement.attachObject(obj(1));
-                dobot_movement.detachObject(obj(2));
+%% Plot and detect color for blue octagon
+[f, v, data] = plyread('blue_octagon.ply', 'tri');
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+octagon_start_position = [0.25, 0.07, 0];
+octagon_scale_factor = 0.002;
+octagon_v_scaled = v * octagon_scale_factor;
+octagon_v_transformation = octagon_v_scaled + octagon_start_position;
+octagon1 = trisurf(f, octagon_v_transformation(:, 1), octagon_v_transformation(:, 2), octagon_v_transformation(:, 3), ...
+    'FaceVertexCData', vertexColours, ...
+    'FaceColor', 'interp', ...
+    'EdgeColor', 'none');
 
-                % Move back up after picking up the object
-                dobot_movement.moveToPosition(robot1, targetPickUpPosition + [0, 0, 0.1]);
-                
-                % Calculate the object's place position with X-axis offset
-                targetPlacePosition = placePositions + [(i-1) * offset, 0, 0];
+[f, v, data] = plyread('blue_octagon.ply', 'tri');
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+octagon_start_position2 = [0.1, 0.25, 0];
+octagon_scale_factor2 = 0.002;
+octagon_v_scaled2 = v * octagon_scale_factor2;
+octagon_v_transformation2 = octagon_v_scaled2 + octagon_start_position2;
+octagon2 = trisurf(f, octagon_v_transformation2(:, 1), octagon_v_transformation2(:, 2), octagon_v_transformation2(:, 3), ...
+    'FaceVertexCData', vertexColours, ...
+    'FaceColor', 'interp', ...
+    'EdgeColor', 'none');
+hideObj(octagon2);
 
-                % Move above the place position to approach safely
-                pause(0.5);
-                dobot_movement.moveToPosition(dobot, targetPlacePosition + [0, 0, 0.1]);
+[f, v, data] = plyread('blue_octagon.ply', 'tri');
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+octagon_start_position3 = [0.1, 0.25, 0];
+octagon_scale_factor3 = 0.002;
+octagon_v_scaled3 = v * octagon_scale_factor3;
+octagon_v_transformation3 = octagon_v_scaled3 + octagon_start_position3;
+octagon3 = trisurf(f, octagon_v_transformation3(:, 1), octagon_v_transformation3(:, 2), octagon_v_transformation3(:, 3), ...
+    'FaceVertexCData', vertexColours, ...
+    'FaceColor', 'interp', ...
+    'EdgeColor', 'none');
+hideObj(octagon3);
 
-                % Move to the exact place position to release the object
-                pause(0.5);
-                dobot_movement.moveToPosition(dobot, targetPlacePosition);
+%% Plot and detect color for red hexagon
+[f, v, data] = plyread('red_hexagon.ply', 'tri');
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+hexagon_start_position = [0.25, -0.07, 0];
+hexagon_scale_factor = 0.002;
+hexagon_v_scaled = v * hexagon_scale_factor;
+hexagon_v_transformation = hexagon_v_scaled + hexagon_start_position;
+hexagon1 = trisurf(f, hexagon_v_transformation(:, 1), hexagon_v_transformation(:, 2), hexagon_v_transformation(:, 3), ...
+    'FaceVertexCData', vertexColours, ...
+    'FaceColor', 'interp', ...
+    'EdgeColor', 'none');
 
-                % Hides end effector obejct and shows final position object
-                dobot_movement.attachObject(obj(2));
-                dobot_movement.detachObject(obj(3));
+[f, v, data] = plyread('red_hexagon.ply', 'tri');
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+hexagon_start_position2 = [0.28, 0.2, 0];
+hexagon_scale_factor2 = 0.002;
+hexagon_v_scaled2 = v * hexagon_scale_factor2;
+hexagon_v_transformation2 = hexagon_v_scaled2 + hexagon_start_position2;
+hexagon2 = trisurf(f, hexagon_v_transformation2(:, 1), hexagon_v_transformation2(:, 2), hexagon_v_transformation2(:, 3), ...
+    'FaceVertexCData', vertexColours, ...
+    'FaceColor', 'interp', ...
+    'EdgeColor', 'none');
+hideObj(hexagon2);
 
-                % Move back up after placing the object
-                pause(0.5);
-                dobot_movement.moveToPosition(dobot, targetPlacePosition + [0, 0, 0.1]);
-            end
-        end
-        
-        % A method to smoothly move the robot to a target position.
-        function moveToPosition(robot1, targetPosition)
+[f, v, data] = plyread('red_hexagon.ply', 'tri');
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+hexagon_start_position3 = [0.28, 0.2, 0];
+hexagon_scale_factor3 = 0.002;
+hexagon_v_scaled3 = v * hexagon_scale_factor3;
+hexagon_v_transformation3 = hexagon_v_scaled3 + hexagon_start_position3;
+hexagon3 = trisurf(f, hexagon_v_transformation3(:, 1), hexagon_v_transformation3(:, 2), hexagon_v_transformation3(:, 3), ...
+    'FaceVertexCData', vertexColours, ...
+    'FaceColor', 'interp', ...
+    'EdgeColor', 'none');
+hideObj(hexagon3);
 
-            % Number of interpolation steps for smooth movement
-            steps = 50;
+%%
+square_position = [0.28, 0, 0.07];
+octagon_position = [0.28, -0.16, 0.07];    % y-coordinate
+hexagon_position = [0.2, -0.28, 0.07];    % y-coordinate
+traj_position = [0.2, 0.15, 0.2];
+place_position1 = [0, 0.2, 0.07];
+place_position2 = [0.2, 0.2, 0.07];
+place_position3 = [0.4, 0.1, 0.07];
 
-            % Calculate the target joint angles using inverse kinematics
-            qTarget = robot1.model.ikine(transl(targetPosition), 'mask', [1 1 1 0 0 0]);
+moveRobotToPosition(robot, square_position);
+hideObj(square1);
+moveRobotToPositionWithSquare(robot, traj_position);
+moveRobotToPositionWithSquare(robot, place_position1);
+showObj(square3);
+moveRobotToPosition(robot, traj_position);
+moveRobotToPosition(robot, octagon_position);
+hideObj(octagon1);
+moveRobotToPositionWithOctagon(robot, traj_position);
+moveRobotToPositionWithOctagon(robot, place_position2);
+showObj(octagon3);
+moveRobotToPosition(robot, traj_position);
+moveRobotToPosition(robot, hexagon_position);
+hideObj(hexagon1);
+moveRobotToPositionWithHexagon(robot, traj_position);
+moveRobotToPositionWithHexagon(robot, place_position3);
+showObj(hexagon3);
+moveRobotToPosition(robot, traj_position);
 
-            % Get the current joint configuration of the robot
-            qCurrent = robot1.model.getpos();
-
-            % Generate a smooth trajectory from the current to target joint configuration
-            qTraj = jtraj(qCurrent, qTarget, steps);
-
-            % Execute the trajectory step-by-step
-            for j = 1:steps
-
-                % Plot the robot's current position in the trajectory
-                robot1.model.plot(qTraj(j, :));
-                pause(0.1);
-            end
-        end
-
-        % Simulate attaching the object to the robot by making it invisible
-        function attachObject(obj)
-            set(obj, 'Visible', 'off'); 
-        end
-
-        % Simulate detaching the object from the robot by making it visible
-        function detachObject(obj)
-            set(obj, 'Visible', 'on'); 
-        end
+%% Function to move the robot from to position
+function moveRobotToPosition(robot, position)
+    steps = 25;
+    currentQ = robot.model.getpos();
+    targetQ = robot.model.ikcon(transl(position), currentQ);
+    traj = jtraj(currentQ, targetQ, steps);
+    for i = 1:steps
+        robot.model.animate(traj(i, :));
+        pause(0.05);
     end
+    pause(0.5);
+end
+
+%% Function to move the robot from to position
+function moveRobotToPositionWithSquare(robot, position)
+    steps = 25;
+    currentQ = robot.model.getpos();
+    targetQ = robot.model.ikcon(transl(position), currentQ);
+    traj = jtraj(currentQ, targetQ, steps);
+    endEffectorPose = robot.model.fkine(currentQ);
+    initial_object_position = endEffectorPose.t';
+    initial_object_position = initial_object_position + [0, 0, -0.05];
+    
+    [f, v, data] = plyread('green_square.ply', 'tri');
+    vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+    square_start_position2 = initial_object_position;
+    square_scale_factor2 = 0.002;
+    square_v_scaled2 = v * square_scale_factor2;
+    square_v_transformation2 = square_v_scaled2 + square_start_position2;
+    square_updated = trisurf(f, square_v_transformation2(:, 1), square_v_transformation2(:, 2), square_v_transformation2(:, 3), ...
+        'FaceVertexCData', vertexColours, ...
+        'FaceColor', 'interp', ...
+        'EdgeColor', 'none');
+
+    for i = 1:steps
+        robot.model.animate(traj(i, :));
+
+        current_q = robot.model.getpos();
+        endEffectorPose = robot.model.fkine(current_q);
+        object_position = endEffectorPose.t';
+        object_position = object_position + [0, 0, -0.05];
+        hideObj(square_updated);
+        [f, v, data] = plyread('green_square.ply', 'tri');
+        vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+        square_start_position2 = object_position;
+        square_scale_factor2 = 0.002;
+        square_v_scaled2 = v * square_scale_factor2;
+        square_v_transformation2 = square_v_scaled2 + square_start_position2;
+        square_updated = trisurf(f, square_v_transformation2(:, 1), square_v_transformation2(:, 2), square_v_transformation2(:, 3), ...
+            'FaceVertexCData', vertexColours, ...
+            'FaceColor', 'interp', ...
+            'EdgeColor', 'none');
+        pause(0.05);
+        showObj(square_updated)
+    end
+    pause(0.5);
+    hideObj(square_updated);
+end
+
+%% Function to move the robot from to position
+function moveRobotToPositionWithOctagon(robot, position)
+    steps = 25;
+    currentQ = robot.model.getpos();
+    targetQ = robot.model.ikcon(transl(position), currentQ);
+    traj = jtraj(currentQ, targetQ, steps);
+    endEffectorPose = robot.model.fkine(currentQ);
+    initial_object_position = endEffectorPose.t';
+    initial_object_position = initial_object_position + [0, 0, -0.05];
+    
+    [f, v, data] = plyread('blue_octagon.ply', 'tri');
+    vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+    octagon_start_position2 = initial_object_position;
+    octagon_scale_factor2 = 0.002;
+    octagon_v_scaled2 = v * octagon_scale_factor2;
+    octagon_v_transformation2 = octagon_v_scaled2 + octagon_start_position2;
+    octagon_updated = trisurf(f, octagon_v_transformation2(:, 1), octagon_v_transformation2(:, 2), octagon_v_transformation2(:, 3), ...
+        'FaceVertexCData', vertexColours, ...
+        'FaceColor', 'interp', ...
+        'EdgeColor', 'none');
+
+    for i = 1:steps
+        robot.model.animate(traj(i, :));
+
+        current_q = robot.model.getpos();
+        endEffectorPose = robot.model.fkine(current_q);
+        object_position = endEffectorPose.t';
+        object_position = object_position + [0, 0, -0.05];
+        hideObj(octagon_updated);
+        [f, v, data] = plyread('blue_octagon.ply', 'tri');
+        vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+        octagon_start_position2 = object_position;
+        octagon_scale_factor2 = 0.002;
+        octagon_v_scaled2 = v * octagon_scale_factor2;
+        octagon_v_transformation2 = octagon_v_scaled2 + octagon_start_position2;
+        octagon_updated = trisurf(f, octagon_v_transformation2(:, 1), octagon_v_transformation2(:, 2), octagon_v_transformation2(:, 3), ...
+            'FaceVertexCData', vertexColours, ...
+            'FaceColor', 'interp', ...
+            'EdgeColor', 'none');
+        pause(0.05);
+        showObj(octagon_updated)
+    end
+    pause(0.5);
+    hideObj(octagon_updated);
+end
+
+%% Function to move the robot from to position
+function moveRobotToPositionWithHexagon(robot, position)
+    steps = 25;
+    currentQ = robot.model.getpos();
+    targetQ = robot.model.ikcon(transl(position), currentQ);
+    traj = jtraj(currentQ, targetQ, steps);
+    endEffectorPose = robot.model.fkine(currentQ);
+    initial_object_position = endEffectorPose.t';
+    initial_object_position = initial_object_position + [0, 0, -0.05];
+    
+    [f, v, data] = plyread('red_hexagon.ply', 'tri');
+    vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+    hexagon_start_position2 = initial_object_position;
+    hexagon_scale_factor2 = 0.002;
+    hexagon_v_scaled2 = v * hexagon_scale_factor2;
+    hexagon_v_transformation2 = hexagon_v_scaled2 + hexagon_start_position2;
+    hexagon_updated = trisurf(f, hexagon_v_transformation2(:, 1), hexagon_v_transformation2(:, 2), hexagon_v_transformation2(:, 3), ...
+        'FaceVertexCData', vertexColours, ...
+        'FaceColor', 'interp', ...
+        'EdgeColor', 'none');
+
+    for i = 1:steps
+        robot.model.animate(traj(i, :));
+
+        current_q = robot.model.getpos();
+        endEffectorPose = robot.model.fkine(current_q);
+        object_position = endEffectorPose.t';
+        object_position = object_position + [0, 0, -0.05];
+        hideObj(hexagon_updated);
+        [f, v, data] = plyread('red_hexagon.ply', 'tri');
+        vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+        hexagon_start_position2 = object_position;
+        hexagon_scale_factor2 = 0.002;
+        hexagon_v_scaled2 = v * hexagon_scale_factor2;
+        hexagon_v_transformation2 = hexagon_v_scaled2 + hexagon_start_position2;
+        hexagon_updated = trisurf(f, hexagon_v_transformation2(:, 1), hexagon_v_transformation2(:, 2), hexagon_v_transformation2(:, 3), ...
+            'FaceVertexCData', vertexColours, ...
+            'FaceColor', 'interp', ...
+            'EdgeColor', 'none');
+        pause(0.05);
+        showObj(hexagon_updated)
+    end
+    pause(0.5);
+    hideObj(hexagon_updated);
+end
+%% Function to hid objects
+function hideObj(obj)
+    obj.Visible = 'off';
+end
+
+%% Function to show objects
+function showObj(obj)
+    obj.Visible = 'on';
 end
